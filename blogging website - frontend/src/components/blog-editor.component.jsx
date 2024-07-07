cc46fb931dcfbb89e7f4111b8b1864cdb52c0304
Hasan Talukder <mahadihasansiam2@gmail.com>
2024-07-07 16:30:54 +0600

    Blog Editor - Banner

--- blogging website - frontend/src/components/blog-editor.component.jsx
+++ blogging website - frontend/src/components/blog-editor.component.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from "../imgs/logo.png"
+import AnimationWrapper from '../common/page-animation'
+import defaultBanner from '../imgs/blog banner.png'
 
 const BlogEditor = () => {
+    const handleBannerUpload = (e) => {
+        console.log(e);
+        let img = e.target.files[0];
+
+        console.log(img)
+    }
   return (
     <>
         <nav className='navbar'>
@@ -22,6 +30,24 @@ const BlogEditor = () => {
             </button>
         </div>
     </nav>
+
+    <AnimationWrapper>
+        <section>
+            <div className='mx-auto max-w-[900px] w-full'>
+
+                <div className='relative aspect-video bg-white hover:opacity-80 border-4 border-grey'>
+                    <label htmlFor="uploadBanner">
+                    <img src={defaultBanner}
+                    className='z-20' alt="" />
+                        <input id='uploadBanner' type='file' accept='.png, .jpg, .jpeg' hidden onChange={handleBannerUpload}
+                        />
+                    </label>
+                </div>
+
+            </div>
+        </section>
+    </AnimationWrapper>
+
     </>
   )
 }
