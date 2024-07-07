3a5c5da4d0cc322cc10fb9b3f57d8492216953d5
Hasan Talukder <mahadihasansiam2@gmail.com>
2024-07-07 16:02:39 +0600

    Section 2 - Editor Page Component

---
+++ blogging website - frontend/src/components/blog-editor.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogEditor = () => {
+  return (
+    <div>
+      <h1>Blog editor</h1>
+    </div>
+  )
+}
+
+export default BlogEditor
--- blogging website - frontend/src/pages/editor.pages.jsx
+++ blogging website - frontend/src/pages/editor.pages.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { UserContext } from '../App'
+import { Navigate } from 'react-router-dom'
+import BlogEditor from '../components/blog-editor.component';
+import PublishForm from '../components/publish-form.component';
 
 const Editor = () => {
+
+    const [ editorState, setEditorState ] = useState("editor");
+
+    let { userAuth: { access_token } } = useContext(UserContext)
+
+
   return (
-    <div>
-      <h1>editor</h1>
-    </div>
+    access_token === null ? <Navigate to="/signin"/> : editorState == "editor" ? <BlogEditor/> : <PublishForm/>
+    
   )
 }
 
