e5db0f1464bfb66a9f0dc67ec1ae704dd7c131d7
Hasan Talukder <mahadihasansiam2@gmail.com>
2024-09-08 17:45:57 +0600

    Home Page - fetching latest blogs

--- blogging website - frontend/src/pages/home.page.jsx
+++ blogging website - frontend/src/pages/home.page.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import AnimationWrapper from '../common/page-animation'
 import InPageNavigation from '../components/inpage-navigation.component'
+import Loader from '../components/loader.component'
 
 const Homepage = () => {
+
+  let [ blogs, setBlog] = useState(null);
+
+  const fetchLatestBlogs = () => {
+    axios.get(import.meta.env.VITE_SERVER_DOMAIN + '/latest-blogs')
+    .then(({ data }) => {
+      setBlog(data.blogs)
+    })
+    .catch (err => {
+      console.log(err)
+    })
+  }
+
+  useEffect(() => {
+    fetchLatestBlogs()
+  })
+
   return (
     <AnimationWrapper>
     
@@ -10,7 +29,16 @@ const Homepage = () => {
       {/* latest blog */}
         <div className=' w-full'>
           <InPageNavigation routes={["home", "trending blogs"]} defaultHidden={["trending blogs"]}>
-            <h1>Latest Blogs Here</h1>
+            
+          <>
+            {
+              blogs === null ? <Loader /> :
+              blogs.map((blogs, i) => {
+                return <h1 key={i}>{ blogs.title }</h1> 
+              })
+            }
+          </>
+
             <h1>old Blogs Here</h1>
           </InPageNavigation>
         </div>
