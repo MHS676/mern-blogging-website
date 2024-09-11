1b9c645f687d32594d4f126276cab1a6c14a6528
Hasan Talukder <mahadihasansiam2@gmail.com>
2024-09-11 18:06:19 +0600

    Pagination

--- blogging website - frontend/src/pages/home.page.jsx
+++ blogging website - frontend/src/pages/home.page.jsx
@@ -6,29 +6,47 @@ import Loader from '../components/loader.component'
 import BlogPostCard from '../components/blog-post.component'
 import MinimalBlogPost from '../components/nobanner-blog-post.component'
 import NoDataMessage from '../components/nodata.component'
+import FilterPaginationData from '../common/filter-pagination-data'
+import LoadMoreDataBtn from '../components/load-more.component'
 
 const Homepage = () => {
 
   let [ blogs, setBlog] = useState(null);
+
   let [ trendingBlogs, setTrendingBlog] = useState(null);
   let [ pageState, setPageState] = useState("home");
 
   let categories = ["programming", "hollywood", "film making", "social media", "cooking", "tech", "finances", "travel", "java"]
 
-  const fetchLatestBlogs = () => {
-    axios.get(import.meta.env.VITE_SERVER_DOMAIN + '/latest-blogs')
-    .then(({ data }) => {
-      setBlog(data.blogs)
+  const fetchLatestBlogs = ( page = 1 ) => {
+    axios.post(import.meta.env.VITE_SERVER_DOMAIN + '/latest-blogs', { page })
+    .then( async ({ data }) => {
+
+
+      let formateData = await FilterPaginationData({
+        state: blogs,
+        data: data.blogs,
+        page,
+        countRoute: '/all-latest-blogs-count'
+      })
+      setBlog(formateData)
     })
     .catch (err => {
       console.log(err)
     })
   }
 
-  const fetchBlogsByCategory = () => {
-    axios.post(import.meta.env.VITE_SERVER_DOMAIN + '/search-blogs', { tag: pageState })
-    .then(({ data }) => {
-      setBlog(data.blogs)
+  const fetchBlogsByCategory = ({ page = 1 }) => {
+    axios.post(import.meta.env.VITE_SERVER_DOMAIN + '/search-blogs', { tag: pageState, page })
+    .then( async ({ data }) => {
+      let formateData = await FilterPaginationData({
+        state: blogs,
+        data: data.blogs,
+        page,
+        countRoute: '/search-blogs-count',
+        data_to_send: { tag: pageState }
+      })
+      setBlog(formateData)
     })
     .catch (err => {
       console.log(err)
@@ -64,10 +82,10 @@ const Homepage = () => {
 
     activeTabRef.current.click();
 
-    if(pageState == 'home') {
-    fetchLatestBlogs()
+    if(pageState === 'home') {
+    fetchLatestBlogs({ page: 1 })
     } else {
-      fetchBlogsByCategory()
+      fetchBlogsByCategory({ page: 1 })
     }
 
     if(!trendingBlogs){
@@ -90,8 +108,8 @@ const Homepage = () => {
           <>
             {
               blogs == null ? ( <Loader /> ): (
-                blogs.length ? 
-              blogs.map((blog, i) => {
+                blogs.results.length ? 
+              blogs.results.map((blog, i) => {
                 return ( 
                   <AnimationWrapper key={i} transition={{ duration: 1 , delay:i*0.1 }}>
                   <BlogPostCard content={blog} author={blog.author.personal_info}/>
@@ -100,6 +118,7 @@ const Homepage = () => {
               })
               : <NoDataMessage message="No blogs published" />
             )}
+            <LoadMoreDataBtn state={blogs} fetchDataFun={( pageState == "home" ? fetchLatestBlogs : fetchBlogsByCategory)}/>
           </>
 
             <>
