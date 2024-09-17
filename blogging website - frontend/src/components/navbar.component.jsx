9b5d2755a94ebc05dd230be34c0e7171df3a9acd
Hasan Talukder <mahadihasansiam2@gmail.com>
2024-09-17 23:33:06 +0600

    Side Navbar

--- blogging website - frontend/src/components/navbar.component.jsx
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className='navbar'>
+      <nav className='navbar z-50'>
         <Link to='/' className="flex-none w-10">
           <img className='w-full' src={logo} alt="Logo" />
         </Link>
